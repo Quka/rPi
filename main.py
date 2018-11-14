47c7a238845a1a8669267fe4f14a8b80e23b3bf3
Arlind Ukshini <quka93@gmail.com>
2018-11-14 09:47:13 +0100

    bg

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import time
 
 sense = SenseHat()
 
-sense.show_message("SEND NUDES", text_colour=[255, 255, 255])
+sense.show_message("SEND NUDES", text_colour=[255, 0, 0], back_colour=[255, 255, 255])
