6349eb562e620eab2b9970e429c89075592cb7a5
Arlind Ukshini <quka93@gmail.com>
2018-11-14 09:54:56 +0100

    clear

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ import time
 
 sense = SenseHat()
 
+count = 0
+
 while(True):
+    if(count == 5)
+        sense_hat.clear()
+        break
+
     sense.show_message("SEND NUDES", text_colour=[255, 0, 0], back_colour=[100, 100, 100])
+    count = count + 1
