6e0f5f2b799a398d3c5c6680ee43e958c8bccf80
Arlind Ukshini <quka93@gmail.com>
2018-11-14 12:35:37 +0100

    refactor

--- sensor.py
+++ sensor.py
@@ -2,32 +2,45 @@ from sense_hat import SenseHat
 import socket
 
 sense = SenseHat()
+timestamp = datetime.now
 
-# Setup UDP socket for broadcasting
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+def setup_udp_socket():
+    # Setup UDP socket for broadcasting
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-# Set a timeout so the socket does not block
-# indefinitely when trying to receive data.
-server.settimeout(0.2)
+    # Set a timeout so the socket does not block
+    # indefinitely when trying to receive data.
+    server.settimeout(0.2)
 
-server.bind(("", 44444))
+    server.bind(("", 44444))
+    return server
+
+def get_sense_data():
+    sense_data = {
+        't': round(sense.get_temperature(), 1),
+        'p': round(sense.get_pressure(), 1),
+        'h': round(sense.get_humidity(), 1),
+        'date': datetime.now()
+    }
+    
+    return sense_data
+
+server = setup_udp_socket()
 
 while(True):
-    t = sense.get_temperature()
-    p = sense.get_pressure()
-    h = sense.get_humidity()
+    data = get_sense_data()
+    time = data[-1] - timestamp # træk timestamp fra datetime i data
 
-    t = round(t, 1)
-    p = round(p, 1)
-    h = round(h, 1)
+    if time.seconds > delay:
+        # The message to send
+        msg = "C = %s, P = %s H = %s" % (t, p, h)
+        # Encode the message to bytes and in utf-8 (default)
+        msgBytes = msg.encode()
 
-    # The message to send
-    msg = "C = %s, P = %s H = %s" % (t, p, h)
-    # Encode the message to bytes and in utf-8 (default)
-    msgBytes = msg.encode()
+        dataBytes = data.encode()
 
-    # Broadcast message to port 37020 via UDP Socket
-    server.sendto(msgBytes, ('<broadcast>', 37020))
+        # Broadcast message to port 37020 via UDP Socket
+        server.sendto(dataBytes, ('<broadcast>', 37020))
 
-    sense.show_message( "sent", scroll_speed=0.05 )
+        sense.show_message( "sent", scroll_speed=0.05 )
