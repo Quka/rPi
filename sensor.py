757f6df69ba21cb0acac08b76a7733f1ba02752f
Arlind Ukshini <quka93@gmail.com>
2018-11-14 11:00:31 +0100

    sensor socket

--- sensor.py
+++ sensor.py
@@ -1,15 +1,30 @@
 from sense_hat import SenseHat
+import socket
 
 sense = SenseHat()
 
+# Setup UDP socket for broadcasting
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+# Set a timeout so the socket does not block
+# indefinitely when trying to receive data.
+server.settimeout(0.2)
+
+server.bind(("", 44444))
 
 while(True):
-  tc = sense.get_temperature()
-  tch = sense.get_temperature_from_humidity()
-  
-  tc = round(tc, 1)
-  tch = round(tch, 1)
-  
-  msg = "TempC = %s, TempHumC = %s" % (tc, tch)
-  
-  sense.show_message( msg, scroll_speed=0.1 )
+    t = sense.get_temperature()
+    p = sense.get_pressure()
+    h = sense.get_humidity()
+
+    t = round(t, 1)
+    p = round(p, 1)
+    h = round(h, 1)
+
+    msg = "C = %s, P = %s H = %s" % (t, p, h)
+
+    sense.show_message( msg, scroll_speed=0.1 )
+
+    # Broadcast message to port 37020 via UDP Socket
+    server.sendto(msg, ('<broadcast>', 37020))
