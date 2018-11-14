052bb6b2ceeea2c1f82983b68e7d41c88514c215
Arlind Ukshini <quka93@gmail.com>
2018-11-14 12:45:31 +0100

    sense_data to data

--- sensor.py
+++ sensor.py
@@ -37,7 +37,7 @@ while(True):
     # Sæt et delay for hvor ofte den skal læse data
     if time.seconds > delay:
         # The message to send
-        msg = "C = %s, P = %s H = %s" % (sense_data["t"], sense_data["p"], sense_data["h"])
+        msg = "C = %s, P = %s H = %s" % (data["t"], data["p"], data["h"])
         # Encode the message to bytes and in utf-8 (default)
         msgBytes = msg.encode()
 
